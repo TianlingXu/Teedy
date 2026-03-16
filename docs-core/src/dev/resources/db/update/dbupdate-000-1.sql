9e9595856f326a3f641b7888fbe630a0887c0b52
TianlingXu <12211752@mail.sustech.edu.cn>
2026-03-16 14:45:04 +0800

    [ADD] visiting as guest button

--- docs-core/src/dev/resources/db/update/dbupdate-000-1.sql
+++ docs-core/src/dev/resources/db/update/dbupdate-000-1.sql
@@ -1,1 +1,2 @@
-update T_CONFIG set CFG_VALUE_C = 'RAM' where CFG_ID_C = 'LUCENE_DIRECTORY_STORAGE';
+-- Use FILE so Lucene index persists across server restarts (dev persistence)
+update T_CONFIG set CFG_VALUE_C = 'FILE' where CFG_ID_C = 'LUCENE_DIRECTORY_STORAGE';
